f1283a69de1fc649f9e3dbafe6e01635e4384c8d
Ilya Boyandin <ilyabo@gmail.com>
2012-03-30 17:03:00 +0200

    segment widths work

--- vis/js/railways.js
+++ vis/js/railways.js
@@ -18,20 +18,48 @@
 
       d3.loadData()
         .json('segments', 'data/edges-sbb.json')
+        .json('stations', 'data/stations-sbb.json')
+        .json('trains','../data/edge_hours.json')
         .onload(function(data) {
           var mapProj = d3.geo.mercator();
+          var hour = 9;
           //mapProj.translate([0,0]);
           //mapProj.scale(1);
           
           fitProjection(mapProj, data.segments, [[0,0],[width, height]], true);
 
           var mapProjPath = d3.geo.path().projection(mapProj);
-          vis.selectAll('path')
+          vis.selectAll('path.segments')
               .data(data.segments.features)
             .enter().append('path')
+              .attr('class', 'segments')
               .attr('d', mapProjPath)
               .attr('stroke', 'black')
               .attr('stroke-opacity', '.5')
-              .attr('fill', 'none');
+              .attr('fill', 'none')
+              .attr('stroke-width', function(d, i) {
+                var edgeid = +d.properties.edge_id;
+                var hours = data.trains[edgeid];
+                if (hours !== undefined  &&  hours[hour] !== undefined) {
+                var forhour = hours[hour]; 
+                  return forhour.length * 2;
+                }
+                else
+                  return 1;
+              })
+              ;
+
+              /*
+          vis.selectAll('path.stations')
+              .data(data.stations.features)
+            .enter().append('path')
+              .attr('class', 'stations')
+              .attr('d', mapProjPath)
+              //.attr('transform', 'scale(0.5)')
+              .attr('stroke', 'black')
+              .attr('stroke-opacity', '.5')
+              .attr('fill', 'blue');
+            */
+
         });
 
