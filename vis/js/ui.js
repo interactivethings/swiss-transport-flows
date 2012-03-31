bad2011ad849a8a44709b64f07e1e200c074ae0a
Benjamin Wiederkehr <me@benjaminwiederkehr.com>
2012-03-31 10:31:11 +0200

    Added panel toggle and basic interaction

---
+++ vis/js/ui.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("a#panel_toggle").click(function() {
+    var panel = $("#panel");
+    if(panel.attr("class") == "folded"){
+      $(".section").not("#title").slideDown();
+      $("#title").removeClass("last");
+      panel.attr("class", "unfolded");
+    }else{
+      $(".section").not("#title").slideUp();
+      $("#title").addClass("last");
+      panel.attr("class", "folded");
+    }
+  });
+});
