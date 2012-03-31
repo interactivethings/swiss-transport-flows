027a144917ebcf2750c331b557d679394f0f68ab
Thomas Preusse <thomas@springadvertising.com>
2012-03-31 15:00:37 +0200

    added auto complete to location field

--- vis/js/init-map.js
+++ vis/js/init-map.js
@@ -104,5 +104,29 @@ $(document).bind('stf-ready', function(){
         });
         return "translate(" + d.x + "," + d.y + ")";
     }
+	
+	 $('#location_txt').autocomplete({
+		source: function(request, response){
+			$.get('http://9kl.be/locations.php?callback=?', {
+				query: request.term,
+				type: 'station'
+			}, function(data){
+				response($.map(data.stations, function(station){
+					return {
+						label: station.name,
+						station: station
+					}
+				}));
+			}, 'json');
+		},
+		select: function(event, ui){
+			console.log(ui.item.station, ui.item.station.coordinate.x, ui.item.station.coordinate.y);
+			map.center({
+		        lat: ui.item.station.coordinate.y,
+		        lon: ui.item.station.coordinate.x
+		    });
+			map.zoom(14);
+		}
+	});
 });
 
